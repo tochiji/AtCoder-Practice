f40b00add1d67d143472d9429cc3ecd4c4ddf9c2
Yosi <yosi.i.job@gmail.com>
2019-12-04 18:23:47 +0900

    🎉 3AC

---
+++ solved/ABC120_A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<(n);i++)
+#define repi(i, n) for (int i = 0; i <= (n); i++)
+#define all(x) (x).begin(), (x).end()
+typedef long long ll;
+using namespace std;
+
+int main() {
+	int A,B,C;
+	cin >> A >> B >> C;
+	if(B/A>C){
+		cout << C << endl;
+	} else {
+		cout << B/A << endl;
+	}
+}
