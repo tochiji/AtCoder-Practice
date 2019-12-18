62dbd0cfab19e700efa9c6f2d9a1716c15aeac63
Yosi <yosi.i.job@gmail.com>
2019-12-18 17:13:08 +0900

    1AC -2進数のやつ

--- ABC105_C.cpp
+++ ABC105_C.cpp
@@ -4,7 +4,49 @@
 typedef long long ll;
 using namespace std;
 
+bitset<40> rec(ll N, bitset<40> ans) {
+    if (N == 0) return ans;
+
+    ll sum = 0;
+    if (N > 0) {
+        int i = 0;
+        while (true) {
+            ll p = pow(2, i);
+            sum += p;
+            if (sum >= N) {
+                N -= p;
+                break;
+            }
+            i += 2;
+        }
+        ans[i] = 1;
+        return rec(N, ans);
+    } else {
+        int i = 1;
+        while (true) {
+            ll p = pow(-2, i);
+            sum += p;
+            if (sum <= N) {
+                N -= p;
+                break;
+            }
+            i += 2;
+        }
+        ans[i] = 1;
+        return rec(N, ans);
+    }
+}
+
 int main() {
-    int N;
+    ll N;
     cin >> N;
+
+    if (N == 0) {
+        cout << 0 << endl;
+        return 0;
+    } else {
+        string b = rec(N, 0).to_string();
+        int top = b.find("1");
+        cout << b.substr(top) << endl;
+    }
 }
