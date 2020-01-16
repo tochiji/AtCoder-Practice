cc65863fc3b99e20ca15e6a97c3d1364052f586b
Yosi <7910558+tochiji@users.noreply.github.com>
2020-01-17 02:44:32 +0900

    いろいろ頑張った

---
+++ solved/ABC101_C.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define all(x) (x).begin(), (x).end()
+typedef long long ll;
+using namespace std;
+
+int main() {
+    int N,K;
+    cin >> N >> K;
+    cout << (N-1+K-2)/(K-1) << endl;
+}
