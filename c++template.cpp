52ddf44f1e0687e52b669fb5f7a83548e514b50e
tochiji <7910558+tochiji@users.noreply.github.com>
2020-10-27 01:04:22 +0900

    update: settings

--- c++template.cpp
+++ c++template.cpp
@@ -1,8 +1,10 @@
 #include <bits/stdc++.h>
+#include <atcoder/all>
 #define rep(i, n) for (ll i = 0; i < (n); i++)
 #define all(x) (x).begin(), (x).end()
 using ll = long long;
 using namespace std;
+using namespace atcoder;
 template <typename T> using vec = std::vector<T>;
 
 int main() {
