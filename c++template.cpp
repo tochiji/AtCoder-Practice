83388537428acb5fbec2333446ece77c89d66551
Yosi <7910558+tochiji@users.noreply.github.com>
2020-12-24 02:41:12 +0900

    feat: updates

--- c++template.cpp
+++ c++template.cpp
@@ -1,11 +1,20 @@
-#include <bits/stdc++.h>
 #include <atcoder/all>
+#include <bits/stdc++.h>
 #define rep(i, n) for (ll i = 0; i < (n); i++)
+#define rrep(i, n) for (ll i = (n); i >= 0; i--)
 #define all(x) (x).begin(), (x).end()
 using ll = long long;
 using namespace std;
 using namespace atcoder;
-template <typename T> using vec = std::vector<T>;
+template <typename T> using vec   = std::vector<T>;
+template <typename T> using vvec  = std::vector<vector<T>>;
+template <typename T> using vvvec = std::vector<vector<vector<T>>>;
+template <class T> void chmax(T &a, T b) { (a < b) && (a = b); }
+template <class T> void chmin(T &a, T b) { (a > b) && (a = b); }
+
+using mint = modint1000000007;
+ostream &operator<<(ostream &os, const mint &a) { return os << a.val(); }
+
 
 int main() {
     //#INPUT
