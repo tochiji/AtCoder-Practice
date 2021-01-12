92b06086e8d5f3eacfb886dc332515d66a7111fc
Yosi <7910558+tochiji@users.noreply.github.com>
2021-01-13 01:04:06 +0900

    feat: update

--- c++template.cpp
+++ c++template.cpp
@@ -3,14 +3,20 @@
 #define rep(i, n) for (ll i = 0; i < (n); i++)
 #define rrep(i, n) for (ll i = (n); i >= 0; i--)
 #define all(x) (x).begin(), (x).end()
-using ll = long long;
 using namespace std;
 using namespace atcoder;
+using ll = long long;
+using Pl = pair<ll, ll>;
+
 template <typename T> using vec   = std::vector<T>;
 template <typename T> using vvec  = std::vector<vector<T>>;
 template <typename T> using vvvec = std::vector<vector<vector<T>>>;
 template <class T> void chmax(T &a, T b) { (a < b) && (a = b); }
 template <class T> void chmin(T &a, T b) { (a > b) && (a = b); }
+
+using mint = modint1000000007;
+ostream &operator<<(ostream &os, const mint &a) { return os << a.val(); }
+
 template <class T> void printv(vec<T> a) {
     for (auto e : a)
         cout << e << " ";
@@ -21,11 +27,6 @@ template <class T> void printvv(vvec<T> a) {
         printv(e);
 }
 
-using mint = modint1000000007;
-ostream &operator<<(ostream &os, const mint &a) { return os << a.val(); }
-
-
 int main() {
     //#INPUT
-
 }
